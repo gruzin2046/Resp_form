f33b72f5abd02ad5c131d74452aa2f5d51326d2c
Albert Łuniewicz <albert2046@wp.pl>
2019-05-01 01:05:31 +0200

    form complete

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 
 
 
+document.addEventListener("DOMContentLoaded", () => {
+
+
+
+
+    // let container = document.querySelector("div");
+    // console.log(data)
+
+    // for (let i = 0; i < data.name.length; i++) {
+    //     let newDiv = document.createElement("div");
+    //     newDiv.classList.add("dataBox");
+    //     container.appendChild(newDiv)
+
+
+    //     //rozwiązanie 1
+    //     button.addEventListener('click', (e) => {
+    //         e.target.parentElement.style.display = 'none'
+    //     })
+
+    //     let nameBoxes = document.querySelectorAll(".dataBox");
+
+    // }
+
+
+})
