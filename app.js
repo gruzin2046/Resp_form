20974a03e4d0c206a96a43359b86d6de43f69d9a
Albert Łuniewicz <albert2046@wp.pl>
2019-05-01 23:55:03 +0200

    the end

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
 
+    const thankYou = () => alert("Dziękujemy za Twoje zgłoszenie!");
 
+    const mainForm = document.querySelector(".mainForm");
+    const btn = document.querySelector("button");
 
+    btn.addEventListener("mouseover", () => mainForm.style.background = "#7f962f");
+    btn.addEventListener("mouseout", () => mainForm.style.background = "rgba(0, 0, 0, 0.8)");
 
-    // let container = document.querySelector("div");
-    // console.log(data)
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const phone = document.querySelector("#phone");
+    const city = document.querySelector("#city");
+    const firstCheckbox = document.querySelector("#firstCheckbox");
+    const secondCheckbox = document.querySelector("#secondCheckbox");
+    const warnings = document.querySelectorAll(".warning p");
 
-    // for (let i = 0; i < data.name.length; i++) {
-    //     let newDiv = document.createElement("div");
-    //     newDiv.classList.add("dataBox");
-    //     container.appendChild(newDiv)
+    btn.addEventListener("click", (event) => {
+        const warningsArray = [];
+        if (!name.value) {
+            warningsArray.push(0);
+            warnings[0].innerText = "Proszę wypełnić pole";
+        }
 
+        if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+            warningsArray.push(1);
+            warnings[1].innerText = "Proszę wypełnić pole";
+        }
 
-    //     //rozwiązanie 1
-    //     button.addEventListener('click', (e) => {
-    //         e.target.parentElement.style.display = 'none'
-    //     })
+        const numbers = /^[-+]?[0-9]+$/;
+        if (!phone.value.match(numbers) || phone.value.toString().length < 6) {
+            warningsArray.push(2);
+            warnings[2].innerText = "Proszę wypełnić pole";
+        }
 
-    //     let nameBoxes = document.querySelectorAll(".dataBox");
+        if (!city.value) {
+            warningsArray.push(3);
+            warnings[3].innerText = "Proszę wypełnić pole";
+        }
 
-    // }
+        if (!firstCheckbox.checked) {
+            warningsArray.push(4);
+            warnings[4].innerText = "Proszę wyrazić zgodę";
+        }
 
+        if (!secondCheckbox.checked) {
+            warningsArray.push(5);
+            warnings[5].innerText = "Proszę wyrazić zgodę";
+        }
 
+        if (warningsArray.length != 0) {
+            event.preventDefault();
+        }
+        else {
+            thankYou();
+        }
+    })
 })
